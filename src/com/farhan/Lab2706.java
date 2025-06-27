fc9ae43ddfcfbaa6a631685dbe8a483736568a95
farhan737 <mdfarhan93933@gmail.com>
2025-06-27 09:59:20 +0530

    27062025

---
+++ src/com/farhan/Lab2706.java
@@ -0,0 +1,11 @@
+package com.farhan;
+
+import com.farhan.others.Book;
+
+public class Lab2706 {
+    public static void main(String[] args) {
+        Book b1 = new Book();
+        System.out.println(b1.printDisc(1, "Shadow Slave", "GuiltyThree", 1000, 14));
+        ;
+    }
+}
