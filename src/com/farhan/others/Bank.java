95d43a1d28f19ab747c2ca95e136e7b0ba77f134
farhan737 <mdfarhan93933@gmail.com>
2025-07-02 11:46:14 +0530

    02072025

---
+++ src/com/farhan/others/Bank.java
@@ -0,0 +1,25 @@
+package com.farhan.others;
+
+public class Bank {
+    static Long accountNumber = 62310100119L;
+    Long accNo;
+    String accountName;
+    Double balance;
+
+    public Bank(String accountName, Double balance) {
+        accountNumber++;
+        accNo = accountNumber;
+        this.accountName = accountName;
+        this.balance = balance;
+    }
+
+    public void printNumber() {
+        System.out.println(accountNumber);
+    }
+
+    public void addAccount() {
+        System.out.println("Account No: " + accNo);
+        System.out.println("Account Name: " + accountName);
+        System.out.println("Account Balance: " + balance);
+    }
+}
