dde74f3ab3ee85fb16628963adfce5671bcfbc88
farhan737 <mdfarhan93933@gmail.com>
2025-07-03 10:05:38 +0530

    03072025

---
+++ src/com/farhan/Lab0307.java
@@ -0,0 +1,10 @@
+package com.farhan;
+
+import com.farhan.others.SimpleCalc;
+
+public class Lab0307 {
+    public static void main(String[] args) {
+        SimpleCalc s = new SimpleCalc();
+        s.subtract();
+    }
+}
