f897b746595893f092e7ee22576dad0df1aa635b
farhan737 <mdfarhan93933@gmail.com>
2025-07-09 09:34:37 +0530

    09072025

--- my-mysql-queries/Lab0107.sql
+++ my-mysql-queries/Lab0107.sql
@@ -1,31 +1,33 @@
 use maindata;
 
 create table course2 (
-courseid int not null,
-coursename varchar(50) not null,
-fee int not null,
-primary key (courseid)
+    courseid int not null,
+    coursename varchar(50) not null,
+    fee int not null,
+    primary key (courseid)
 );
 
 create table student1 (
-studentid int not null,
-studentname varchar(50) not null,
-mobileno varchar(50) not null,
-courseid int not null,
-foreign key (courseid) references course2(courseid) on update cascade
+    studentid int not null,
+    studentname varchar(50) not null,
+    mobileno varchar(50) not null,
+    courseid int not null,
+    foreign key (courseid) references course2 (courseid) on update cascade
 );
 
-insert into course2 values 
-(1, 'pfs', 30000),
-(2, 'jfs', 32000),
-(3, 'CC', 29000),
-(4, 'dotnet', 35000);
+insert into
+    course2
+values (1, 'pfs', 30000),
+    (2, 'jfs', 32000),
+    (3, 'CC', 29000),
+    (4, 'dotnet', 35000);
 
-insert into student1 values 
-(1, 'farhan', '9393396691', 2),
-(2, 'pushkar', '9292296691', 4),
-(3, 'vema', '9191197791', 2),
-(4, 'hari', '9898898893', 1);
+insert into
+    student1
+values (1, 'farhan', '9393396691', 2),
+    (2, 'pushkar', '9292296691', 4),
+    (3, 'vema', '9191197791', 2),
+    (4, 'hari', '9898898893', 1);
 
 select * from course2;
 
--- my-mysql-queries/Lab0907.sql
+++ my-mysql-queries/Lab0907.sql
@@ -14,5 +14,23 @@ VALUES ('farhan', 18),
 
 SELECT * FROM customer1;
 
+DROP TABLE customer1;
+
+USE maindata;
+
+CREATE TABLE customer1 (
+    custid int AUTO_INCREMENT,
+    name VARCHAR(100) NOT NULL,
+    age INT NOT NULL CHECK (age <= 18),
+    PRIMARY KEY (custid)
+);
+
+INSERT INTO
+    customer1 (name, age)
+VALUES ('farhan', 18),
+    ('pushkar', 17);
+
+SELECT * FROM customer1;
+
 --hello
 DROP TABLE customer1;
