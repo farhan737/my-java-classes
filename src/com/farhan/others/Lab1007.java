eede169b8b4ea1f7108c53131d302330fc0d6942
farhan737 <mdfarhan93933@gmail.com>
2025-07-10 09:49:49 +0530

    10072025

---
+++ src/com/farhan/others/Lab1007.java
@@ -0,0 +1,12 @@
+package com.farhan.others;
+
+public class Lab1007 {
+    public static void main(String[] args) {
+        MultiConstructor m1 = new MultiConstructor("farhan");
+        m1.display();
+        MultiConstructor m2 = new MultiConstructor("pushkar", 12);
+        m2.display();
+        MultiConstructor m3 = new MultiConstructor("vema", 14, "azure cloud");
+        m3.display();
+    }
+}
