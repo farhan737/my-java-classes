f0bfcde70e1389fcaea2e9fa53cf601e14a3053c
farhan737 <mdfarhan93933@gmail.com>
2025-07-29 11:46:58 +0530

    test2907

--- my-mysql-queries/Lab2907.sql
+++ my-mysql-queries/Lab2907.sql
@@ -1,7 +1,7 @@
 USE maindata;
 
 --display the employee name, job, salary, department name and location of those employees whose salary is higher than the avg salary of all emplyees working in 'NEW YORK'
-
+--test change
 SELECT e.ename, e.job, e.sal, d.dname, d.loc
 FROM emp AS e
     INNER JOIN dept as d ON d.deptno = e.deptno
