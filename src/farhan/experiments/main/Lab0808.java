167bc8ca18b3c71932bddda0ee01a16b3f58d467
farhan737 <mdfarhan93933@gmail.com>
2025-08-08 11:59:10 +0530

    added sql script to drop, create and insert dept and emp tables

---
+++ src/farhan/experiments/main/Lab0808.java
@@ -0,0 +1,10 @@
+package farhan.experiments.main;
+
+import farhan.util.Patterns;
+
+public class Lab0808 extends Patterns{
+	public static void main(String [] args) {
+		Lab0808 l1 = new Lab0808();
+		l1.printSquareWithNumbers(10);
+	}
+}
--- src/farhan/util/Patterns.java
+++ src/farhan/util/Patterns.java
@@ -1,5 +1,19 @@
 package farhan.util;
 
 public class Patterns {
-
+	public void printSquareWithNumbers(int side) {
+//		example:
+//		input: 5
+//		output:
+//		1 	2 	3 	4 	5 	
+//		6 	7 	8 	9 	10 	
+//		11 	12 	13 	14 	15 	
+//		16 	17 	18 	19 	20 	
+//		21 	22 	23 	24 	25 
+		for (int i = 1; i <= side * side; i++) {
+			System.out.print(i + " \t");
+			if (i % side == 0)
+				System.out.println();
+		}
+	}
 }
